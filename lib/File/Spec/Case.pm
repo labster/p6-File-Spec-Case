2ac671af5fe066339b4c8ef20bba91c80ac325aa
Brent Laabs <bslaabs@gmail.com>
2013-04-13 16:54:36 -0700

    already working.  woo!  add sensitive, insensitive aliases

--- lib/File/Spec/Case.pm
+++ lib/File/Spec/Case.pm
@@ -1,23 +1,28 @@
 class File::Spec::Case;
 
-method default_case_tolerant ($OS = $*OS) {
+use File::Spec;
+
+method default-case-tolerant ($OS = $*OS) {
 	so $OS eq any <MacOS Mac VMS darwin Win32 MSWin32 os2 dos NetWare symbian cygwin Cygwin epoc>;
 }
 
-method always_case_tolerant  ($OS = $*OS) {
+method always-case-tolerant  ($OS = $*OS) {
 	so $OS eq any <MacOS Mac VMS os2 dos>;
 }
 
-method case-tolerant (Str:D $path = $*CWD, $write_ok as Bool = True ) {
-	# This code should be platform independent, but feel free to add local override
+method sensitive(|c)   { not self.tolerant( |c ) }
+method insensitive(|c) {     self.tolerant( |c ) }
+
+method tolerant (Str:D $path = $*CWD, $write_ok as Bool = True ) {
+	return True if self.always-case-tolerant($*OS);
 
 	$path.IO.e or fail "Invalid path given";
-	my @dirs = self.splitdir(self.rel2abs($path));
+	my @dirs = File::Spec.splitdir(File::Spec.rel2abs($path));
 	my @searchabledirs;
 
 	# try looking at each component of $path to see if has letters
 	loop (my $i = +@dirs; $i--; $i <= 0) {
-		my $p = self.catdir(@dirs[0..$i]);
+		my $p = File::Spec.catdir(@dirs[0..$i]);
 		push(@searchabledirs, $p) if $p.IO.d;
 
 		last if $p.IO.l;
@@ -39,8 +44,8 @@ method case-tolerant (Str:D $path = $*CWD, $write_ok as Bool = True ) {
 	# If we couldn't find anything suitable, try writing a test file
 	if $write_ok {
 		for @searchabledirs.grep({.IO.w}) -> $d {
-			my $filelc = self.catdir( $d, 'filespec.tmp');  #because 8.3 filesystems...
-			my $fileuc = self.catdir( $d, 'FILESPEC.TMP');
+			my $filelc = File::Spec.catdir( $d, 'filespec.tmp');  #because 8.3 filesystems...
+			my $fileuc = File::Spec.catdir( $d, 'FILESPEC.TMP');
 			if $filelc.IO.e or $fileuc.IO.e { die "Wait, where did the file matching <alpha> come from??"; }
 			try {
 				spurt $filelc, 'temporary test file for p6 File::Spec, feel free to delete';
@@ -53,14 +58,14 @@ method case-tolerant (Str:D $path = $*CWD, $write_ok as Bool = True ) {
 	}
 
 	# Okay, we don't have write access... give up and just return the platform default
-	return self.default-case-tolerant;
+	return self.default-case-tolerant($*OS);
 
 }
 
 method !case-tolerant-folder( \updirs, $curdir ) {
-	return False unless self.catdir( |updirs, $curdir.uc).IO.e
-			 && self.catdir( |updirs, $curdir.lc).IO.e;
-	return +dir(self.catdir(|updirs)).grep(/:i ^ $curdir $/) <= 1;
+	return False unless File::Spec.catdir( |updirs, $curdir.uc).IO.e
+			 && File::Spec.catdir( |updirs, $curdir.lc).IO.e;
+	return +dir(File::Spec.catdir(|updirs)).grep(/:i ^ $curdir $/) <= 1;
 	# this could be faster by comparing inodes of .uc and .lc
 	# but we can't guarantee POSIXness of every platform that calls this
 }
