2d777570a2897cf4e88b3344383e2a08c5620fd8
Brent Laabs <blaabs@perceptyx.com>
2014-07-11 22:06:30 -0700

    unbust tests by s/File::Spec/IO::Spec/

--- lib/File/Spec/Case.pm
+++ lib/File/Spec/Case.pm
@@ -1,7 +1,5 @@
 class File::Spec::Case;
 
-use File::Spec;
-
 method default-case-tolerant ($OS = $*OS) {
 	so $OS eq any <MacOS Mac VMS darwin Win32 MSWin32 os2 dos NetWare symbian cygwin Cygwin epoc>;
 }
@@ -13,16 +11,17 @@ method always-case-tolerant  ($OS = $*OS) {
 method sensitive(|c)   { not self.tolerant( |c ) }
 method insensitive(|c) {     self.tolerant( |c ) }
 
-method tolerant (Str:D $path = ~$*CWD, :$no_write = False ) {
+method tolerant (Cool:D $path is copy = ~$*CWD, :$no_write = False ) {
 	return True if self.always-case-tolerant($*OS);
 
-	$path.IO.e or fail "Invalid path given";
-	my @dirs = File::Spec.splitdir(File::Spec.rel2abs($path));
+    $path = $path.path;
+	$path.e or fail "Invalid path given";
+	my @dirs = IO::Spec.splitdir(IO::Spec.rel2abs($path));
 	my @searchabledirs;
 
 	# try looking at each component of $path to see if has letters
 	loop (my $i = +@dirs; $i--; $i <= 0) {
-		my $p = File::Spec.catdir(@dirs[0..$i]);
+		my $p = IO::Spec.catdir(@dirs[0..$i]);
 		push(@searchabledirs, $p) if $p.IO.d;
 
 		last if $p.IO.l;
@@ -47,11 +46,11 @@ method tolerant (Str:D $path = ~$*CWD, :$no_write = False ) {
 			# we already know all of these dirs don't contain <alpha>,
 			# so pick a random 8.3 name to avoid race conditions
 			my $tmpname = "{('a'..'z').pick(8).join}.tmp";
-			my $filelc = File::Spec.catdir( $d, $tmpname   );  
-			my $fileuc = File::Spec.catdir( $d, $tmpname.uc);
+			my $filelc = IO::Spec.catdir( $d, $tmpname   );  
+			my $fileuc = IO::Spec.catdir( $d, $tmpname.uc);
 			try {
 				spurt $filelc, :createonly,
-					'temporary test file for p6 File::Spec, feel free to delete';
+					'temporary test file for p6 IO::Spec, feel free to delete';
 				my $result = $fileuc.IO.e;
 				unlink $filelc;
 				return $result;
@@ -66,9 +65,9 @@ method tolerant (Str:D $path = ~$*CWD, :$no_write = False ) {
 }
 
 method !case-tolerant-folder( \updirs, $curdir ) {
-	return False unless File::Spec.catdir( |updirs, $curdir.uc).IO.e
-			 && File::Spec.catdir( |updirs, $curdir.lc).IO.e;
-	return +dir(File::Spec.catdir(|updirs)).grep(/:i ^ $curdir $/) <= 1;
+	return False unless IO::Spec.catdir( |updirs, $curdir.uc).IO.e
+			 && IO::Spec.catdir( |updirs, $curdir.lc).IO.e;
+	return +dir(IO::Spec.catdir(|updirs)).grep(/:i ^ $curdir $/) <= 1;
 	# this could be faster by comparing inodes of .uc and .lc
 	# but we can't guarantee POSIXness of every platform that calls this
 }
